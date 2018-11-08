8768b60fded6200f36557976be8806f0f47315bd
Bingo Bobo <matmazur90@gmail.com>
2018-11-08 11:29:02 +0100

    first commit - added fresh spring project with AOP components

---
+++ src/main/java/com/matmazur/springaopintro/SpringAopIntroApplication.java
@@ -0,0 +1,12 @@
+package com.matmazur.springaopintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopIntroApplication.class, args);
+    }
+}
