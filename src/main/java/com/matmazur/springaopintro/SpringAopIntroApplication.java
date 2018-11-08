a14ff441650235d4b785a959606aa6e68b6e8446
Bingo Bobo <matmazur90@gmail.com>
2018-11-08 12:33:47 +0100

    created POJO Person

--- src/main/java/com/matmazur/springaopintro/SpringAopIntroApplication.java
+++ src/main/java/com/matmazur/springaopintro/SpringAopIntroApplication.java
@@ -2,11 +2,17 @@ package com.matmazur.springaopintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringAopIntroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAopIntroApplication.class, args);
+
+
+        ConfigurableApplicationContext context = SpringApplication.run(SpringAopIntroApplication.class, args);
+
+
+
     }
 }
---
+++ src/main/java/com/matmazur/springaopintro/model/Person.java
@@ -0,0 +1,51 @@
+package com.matmazur.springaopintro.model;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private int id;
+
+
+    public Person(String name, String surname, int id) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
